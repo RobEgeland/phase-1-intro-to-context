7f1f2acc4bc7255bbad80049a9b34353afec2949
Robert Egeland <robegeland@yahoo.com>
2022-01-26 16:34:47 -0600

    commit tree 7ab46aad13687fb0a9379615468ba82c5c124624

--- index.js
+++ index.js
@@ -1,1 +1,70 @@
-// Your code here
+
+
+function createEmployeeRecord(array) {
+    let Employee = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return Employee
+}
+
+function createEmployeeRecords(array) {
+    return array.map(rec => createEmployeeRecord(rec))
+}
+
+function createTimeInEvent(record, date) {
+    let splitDate = date.split(" ")
+    let inEvent = {
+        type: 'TimeIn',
+        date: splitDate[0],
+        hour: parseInt(splitDate[1])
+    }
+    record.timeInEvents.push(inEvent)
+    return record
+}
+
+function createTimeOutEvent(record, date) {
+    let splitDate = date.split(" ")
+    let outEvent = {
+        type: 'TimeOut',
+        date: splitDate[0],
+        hour: parseInt(splitDate[1])
+    }
+    record.timeOutEvents.push(outEvent)
+    return record
+}
+
+function hoursWorkedOnDate(record, date) {
+  let inEvent = record.timeInEvents.find(inEvent => inEvent.date === date)
+  let outEvent = record.timeOutEvents.find(outEvent => outEvent.date === date)
+  return (outEvent.hour - inEvent.hour) / 100
+}
+
+function wagesEarnedOnDate(record, date) {
+    return hoursWorkedOnDate(record, date) * record.payPerHour
+}
+
+function allWagesFor(record) {
+    let sum = 0;
+    for(let i = 0; i < record.timeInEvents.length; i++) {
+        const wagesPerDate = wagesEarnedOnDate(record, record.timeInEvents[i].date)
+        sum += wagesPerDate
+    }
+    return sum
+}
+
+function calculatePayroll(employees) {
+    let sum = 0
+    employees.forEach(employee => {
+        sum += allWagesFor(employee)
+    })
+    return sum
+
+}
+
+
+
